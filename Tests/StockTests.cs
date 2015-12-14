2fd5b5e89518cc18d202fed860b993aafbef6707
David Thibault <thibault.david@gmail.com>
2015-12-14 00:28:39 -0500

    Add some basic unit tests

---
+++ Tests/StockTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stockfighter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    // Note that these tests assume that Stockfighter's test exchange is up and functionning as expected.
+
+    [TestClass]
+    public class StockTests
+    {
+        [TestMethod]
+        public async Task CanGetStockQuote()
+        {
+            var stock = new Stock("TESTEX", "FOOBAR");
+            var quote = await stock.GetQuote();
+
+            Assert.IsNotNull(quote);
+        }
+
+        [TestMethod]
+        public async Task CanGetStockOrderbook()
+        {
+            var stock = new Stock("TESTEX", "FOOBAR");
+            var book = await stock.GetOrderbook();
+
+            Assert.IsNotNull(book);
+        }
+    }
+}
---
+++ Tests/VenueTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stockfighter;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    // Note that these tests assume that Stockfighter's test exchange is up and functionning as expected.
+
+    [TestClass]
+    public class VenueTests
+    {
+        [TestMethod]
+        public async Task CanGetVenueHeartbeat()
+        {
+            var venue = new Venue("TESTEX");
+            var isUp = await venue.IsUp();
+
+            Assert.IsTrue(isUp);
+        }
+
+        [TestMethod]
+        public async Task CanGetVenueStocks()
+        {
+            var venue = new Venue("TESTEX");
+            var stocks = await venue.GetStocks();
+
+            Assert.AreEqual("FOOBAR", stocks[0].Symbol);
+        }
+    }
+}
